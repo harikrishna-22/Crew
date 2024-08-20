aa4f456a2a498a04c2cb01fe67f819a81f61bc6e
harikrishna-22 <harikrishna.s2000@gmail.com>
2024-08-19 19:00:42 -0700

    Initial Commit

---
+++ CrewApp/CrewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrewAppApp.swift
+//  CrewApp
+//
+//  Created by Crew on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CrewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
